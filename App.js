385a31b058def0c41550d6749169c9613240de96
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2020-11-22 18:35:55 +0000

    basic todo list created

--- App.js
+++ App.js
@@ -1,69 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'Neil', id: '1' },
-    { name: 'Caoimhe', id: '2' },
-    { name: 'Nathan', id: '3' },
-    { name: 'Alison', id: '4' },
-    { name: 'Yvonne', id: '5' },
-    { name: 'Higgy', id: '6' },
-    { name: 'Mark', id: '7' },
-  ])
 
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id);
-
-    })
-  }
+  const [todos, setTodos] = useState([
+    {  text: 'call Bristol Engergy', key: '1'  },
+    {  text: 'call Plusnet', key: '2' },
+    {  text: 'buy concertina folders', key: '3' }
+  ]);
 
   return (
     <View style={styles.container}>
-
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id }
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
+      <Header />  
+      <View style={styles.content}>
         
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+            <Text>{item.text}</Text>
 
-        )}
-      
-      />
+            )}
+          
+          />
+        </View>
 
-      {/* <ScrollView>
-
-        {people.map(item => (
-          <View id={item.id}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
+      </View>
 
     </View>
-  );
+  );  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
+    backgroundColor: '#fff', 
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 40,
   }
 });
