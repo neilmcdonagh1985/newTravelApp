2c558c12a3bc698ef1d291fec796a5f73375d227
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2020-11-24 22:18:32 +0000

    add function to update and display current state

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
 
@@ -17,10 +18,21 @@ export default function App() {
     })
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos
+      ]
+
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header />  
       <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
         
         <View style={styles.list}>
           <FlatList
